70f4b10a6b00124a930cba470f4fc65ea27ae695
inderjeet <horainderjeet1@gmail.com>
2025-06-08 14:15:10 +0530

    added new commit

---
+++ src/test/java/com/dev/java/com/dev/java/DevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.java.com.dev.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
