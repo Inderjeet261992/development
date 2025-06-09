ada95f31745b7b3fe71c6e219b4fbb8c36d35aba
Inderjeet261992 <inderjeetkaurhorahora@gmail.com>
2025-06-09 21:48:11 +0530

    Update DevelopmentApplication.java

--- src/main/java/com/dev/java/com/dev/java/DevelopmentApplication.java
+++ src/main/java/com/dev/java/com/dev/java/DevelopmentApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class DevelopmentApplication extends SpringBootServletInitializer {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(EurekaServerApplication.class);
-    }
+public class DevelopmentApplication {
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DevelopmentApplication.class, args);
 	}
